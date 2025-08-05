3d6d450086cd301a56c7215cda45a4c3a91646d7
LOLO <56169175+lolo02afon@users.noreply.github.com>
2025-08-05 18:35:00 +0300

    refactor: fetch discussion history for reactions

--- internal/reaction/routes.go
+++ internal/reaction/routes.go
@@ -1,7 +1,15 @@
 package reaction
 
-// func SetupRoutes(r *gin.RouterGroup, db *storage.DB, commentDB *storage.CommentDB) {
-// 	handler := NewHandler(db, commentDB)
-// 	r.POST("/send", handler.SendReaction)
-// 	log.Printf("[ROUTER] Reaction routes registered")
-// }
+import (
+	"log"
+
+	"atg_go/pkg/storage"
+	"github.com/gin-gonic/gin"
+)
+
+// SetupRoutes регистрирует маршруты для работы с реакциями.
+func SetupRoutes(r *gin.RouterGroup, db *storage.DB, commentDB *storage.CommentDB) {
+	handler := NewHandler(db, commentDB)
+	r.POST("/send", handler.SendReaction)
+	log.Printf("[ROUTER] Reaction routes registered")
+}
