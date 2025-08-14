42c8a5536381514c3713a380420661fca6cbea44
LOLO <56169175+lolo02afon@users.noreply.github.com>
2025-08-14 12:01:33 +0300

    Добавлен механизм блокировки аккаунтов

--- pkg/telegram/reaction.go
+++ pkg/telegram/reaction.go
@@ -10,6 +10,7 @@ import (
 	"atg_go/models"
 	"atg_go/pkg/storage"
 	module "atg_go/pkg/telegram/module"
+	accountdeadlock "atg_go/pkg/telegram/module/account_deadlock"
 
 	"github.com/gotd/td/tg"
 )
@@ -22,6 +23,13 @@ import (
 // ошибку. При неудаче оба идентификатора равны 0.
 func SendReaction(db *storage.DB, accountID int, phone, channelURL string, apiID int, apiHash string, msgCount int, proxy *models.Proxy) (int, int, error) {
 	log.Printf("[START] Отправка реакции в канал %s от имени %s", channelURL, phone)
+
+	// Захватываем мьютекс для аккаунта, чтобы исключить параллельное использование
+	if err := accountdeadlock.LockAccount(accountID); err != nil {
+		return 0, 0, err
+	}
+	// Освобождаем мьютекс по завершении работы
+	defer accountdeadlock.UnlockAccount(accountID)
 
 	username, err := module.Modf_ExtractUsername(channelURL)
 	if err != nil {
