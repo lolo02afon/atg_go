aa3a81d251b8144c0cfee2b8c886be7157b6242e
LOLO <56169175+lolo02afon@users.noreply.github.com>
2025-08-14 23:34:39 +0300

    feat: add dispatcher activity cancel endpoint

--- internal/module/routes.go
+++ internal/module/routes.go
@@ -10,5 +10,6 @@ import (
 func SetupRoutes(r *gin.RouterGroup, db *storage.DB) {
 	handler := NewHandler(db)
 	r.POST("/dispatcher_activity", handler.DispatcherActivity)
+	r.POST("/dispatcher_activity/cancel_all", handler.CancelAllDispatcherActivity)
 	r.POST("/unsubscribe", handler.Unsubscribe)
 }
