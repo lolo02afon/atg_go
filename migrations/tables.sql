b19c264cf9a4dad32f380318624e6921a924a9fd
lolo02afon <ledo02@yandex.ru>
2025-08-16 15:57:28 +0300

    обн 16

--- migrations/tables.sql
+++ migrations/tables.sql
@@ -81,12 +81,3 @@ CREATE TABLE IF NOT EXISTS account_session (
     data_json TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS statistics (
-    id SERIAL PRIMARY KEY,
-    stat_date DATE NOT NULL UNIQUE,             -- Дата, за которую рассчитана статистика (по времени Москвы)
-    comment_mean DOUBLE PRECISION NOT NULL,
-    reaction_mean DOUBLE PRECISION NOT NULL,
-    account_floodban INTEGER NOT NULL,
-    account_all INTEGER NOT NULL
-);
-
