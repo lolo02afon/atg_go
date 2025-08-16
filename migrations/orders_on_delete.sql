a1465cfd0ae287d07a90780790eb95ff806b3fd4
lolo02afon <ledo02@yandex.ru>
2025-08-16 19:14:38 +0300

    обн 1

--- migrations/orders_on_delete.sql
+++
@@ -1,7 +0,0 @@
--- Обновление внешнего ключа order_id для установки NULL при удалении заказа
-ALTER TABLE accounts
-    DROP CONSTRAINT IF EXISTS accounts_order_id_fkey;
-
-ALTER TABLE accounts
-    ADD CONSTRAINT accounts_order_id_fkey
-        FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE SET NULL;
