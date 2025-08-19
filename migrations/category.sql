dcb297ee5cdfbadac96903bd027205868cb3d69c
LOLO <56169175+lolo02afon@users.noreply.github.com>
2025-08-19 23:06:08 +0300

    Удалена таблица category

--- migrations/category.sql
+++
@@ -1,5 +0,0 @@
--- Таблица категорий
-CREATE TABLE IF NOT EXISTS category (
-    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY, -- современный автоинкремент
-    name VARCHAR(64) NOT NULL UNIQUE                    -- Короткое уникальное название категории
-);
