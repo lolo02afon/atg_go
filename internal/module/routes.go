01cc32acda55cba19aa2c3f813f88c02146e4361
LOLO <56169175+lolo02afon@users.noreply.github.com>
2025-09-03 22:28:11 +0300

    feat: обработка расписания публикаций

--- internal/module/routes.go
+++ internal/module/routes.go
@@ -15,6 +15,7 @@ func SetupRoutes(r *gin.RouterGroup, db *storage.DB) {
 	r.POST("/dispatcher_activity/cancel_all", handler.CancelAllDispatcherActivity)
 	r.POST("/unsubscribe", handler.Unsubscribe)
 	r.POST("/order/link_updat", handler.OrderLinkUpdate)
+	r.POST("/channel_duplicate/:id/post_count_day", handler.UpdateChannelDuplicateTimes)
 	authcheck.SetupRoutes(r.Group("/account_auth_check"), db)
 	activesessions.SetupRoutes(r.Group("/active_sessions_disconnect"), db)
 }
